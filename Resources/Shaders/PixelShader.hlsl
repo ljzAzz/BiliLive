5559bf49e6cd7c704ffc8913bbdf75644f4e5a99
ljzAzz <127233878+ljzAzz@users.noreply.github.com>
2026-05-04 14:21:43 +0800

    Update PixelShader.hlsl

--- Resources/Shaders/PixelShader.hlsl
+++ Resources/Shaders/PixelShader.hlsl
@@ -5,13 +5,13 @@
 #define COLOR_MATRIX_BT2020_LIMITED 4
 #define COLOR_MATRIX_COUNT          5
 
-// 常量缓冲区只需要一个索引
+
 cbuffer ColorConvParams : register(b0)
 {
     uint matrixIndex;
 };
 
-// 预先定义转换矩阵 (每个矩阵是3个float4)
+
 static const float4 g_matrices[COLOR_MATRIX_COUNT][3] = {
     // BT.601 Limited -> Full Range RGB
     {
@@ -56,17 +56,12 @@ SamplerState samplerState : register(s0);
 
 float4 main(VertexOutPut vertex) : SV_TARGET
 {
-    // 1. 采样 NV12 (与之前相同)
+
     float Y = texY.Sample(samplerState, vertex.uv).r;
     float2 UV = texUV.Sample(samplerState, vertex.uv).rg;
-    //float2 UV = float2(0.5, 0.5);
-    // 2. 如果矩阵为 Limited 类型，已在矩阵中包含了范围映射，无需额外步骤
-    //    (我们上面定义的矩阵已经将 16-235 YUV 映射到了 0-1 RGB)
-    //    若使用 Full 矩阵，则采样值本身已是 0-1 范围。
 
     float4 yuv = float4(Y, UV.x, UV.y, 1.0);
 
-    // 3. 选择矩阵并计算 RGB
     //uint idx = clamp(matrixIndex, 0, COLOR_MATRIX_COUNT - 1);
     uint idx = 3;
     float4 rgba;
